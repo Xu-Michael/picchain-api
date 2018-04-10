16a404666a07c3b4430d61d39ffe1e6d61fc78c9
Lauren Emms <lauren.emms@live.co.uk>
2018-04-10 14:44:16 +0800

    Added model relations and began to add validations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,7 @@
 class Location < ApplicationRecord
+  has_many :pins, dependent: :destroy
+  has_many :users, through: :pins
+  validates :lng, presence: true, uniqueness: true
+  validates :lat, presence: true, uniqueness: true
+  validates :address, presence: true, uniqueness: true
 end
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,7 @@
 class Pin < ApplicationRecord
   belongs_to :location
   belongs_to :user
+  validates :image, presence: true, uniqueness: { scope: :user }
+  validates :latitude, presence: true
+  validates :longitude, presence: true
 end
